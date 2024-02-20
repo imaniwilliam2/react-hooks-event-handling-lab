0092cb302201cbdcf099541baeb9135e8c184a8b
imaniwilliam2 <154285022+imaniwilliam2@users.noreply.github.com>
2024-02-20 15:29:36 -0500

    all testing passing

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
 // Code EyesOnMe Component Here
+
+function EyesOnMe(){
+
+    function focusEvent() {
+        console.log('Good!')
+    }
+    function blurEvent() {
+        console.log('Hey! Eyes on me!')
+    }
+
+
+    return ( 
+        <div>
+            <button onFocus={focusEvent} onBlur={blurEvent}>Eyes on me</button>
+        </div>
+    )
+}
+
+export default EyesOnMe
